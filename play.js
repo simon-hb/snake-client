00869e7dcd18621f7d4977814b17427366339869
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-01 18:02:54 +0000

    added input file

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
-
-const handleUserInput = function() {
-  const stdin = process.stdin;
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-}
 
 setupInput();
