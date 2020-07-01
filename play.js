dbfc3b61cb57ee147cabda4dd6a6d70c3dc0fef3
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-01 17:58:42 +0000

    added handleUserInput and setupInput unfctions to play.js

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+setupInput();
