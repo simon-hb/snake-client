05271431c969fbd8c8e082aaaa68e3a2766f1963
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-01 20:57:12 +0000

    snake can now move

--- input.js
+++ input.js
@@ -1,18 +1,40 @@
-const handleUserInput = function() {
+const handleUserInput = function(conn) {
   const stdin = process.stdin;
+  
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
     }
   })
+  stdin.on('data', (key) => {
+    if (key === 'w') {
+      conn.write('Move: up');
+    }
+  })
+  stdin.on('data', (key) => {
+    if (key === 'a') {
+      conn.write('Move: left');
+    }
+  })
+  stdin.on('data', (key) => {
+    if (key === 's') {
+      conn.write('Move: down');
+    }
+  })
+  stdin.on('data', (key) => {
+    if (key === 'd') {
+      conn.write('Move: right');
+    }
+  })
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  handleUserInput();
+  handleUserInput(connection);
   return stdin;
 }
 
